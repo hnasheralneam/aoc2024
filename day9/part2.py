7633b6276a22fad635796a727f1435091379b9e2
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2024-12-09 02:11:32 -0500

    Day 9 Part 2 Progress

--- day9/part2.py
+++ day9/part2.py
@@ -2,19 +2,28 @@ data = []
 is_data = True
 id_num = 0
 
+empty_spaces = []
+blocks = []
+
 f = open("day9/input.txt", "r").readlines()[0]
 
-def get_empty_before(i):
-    index = data.index(-1)
-    if index >= i:
-        return -1
-    else:
-        return index
+def spot_available(size):
+    for i in range(0, len(empty_spaces)):
+        space = empty_spaces[i]
+        if len(space) >= size:
+            empty_spaces.pop(i)
+            return space
+    return -1
 
 def swap(first, second):
     temp = data[first]
     data[first] = data[second]
     data[second] = temp
+
+def move(block, empty_spot):
+    # both block and empty_spot are in format [startIndex, length]
+    for i in range(0, empty_spot[1]):
+        swap(empty_spot[0] + i, block[0] + i)
 
 for num in f:
     for i in range(0, int(num)):
@@ -23,16 +32,16 @@ for num in f:
             value = id_num
         data.append(int(value))
     if is_data:
+        blocks.append([len(data) - 1, int(num)])
         id_num += 1
+    else:
+        empty_spaces.append([len(data) - 1, int(num)])
     is_data = not is_data
-
-for i in range(len(data) - 1, -1, -1):
-    if not data[i] == -1:
-        empty_spot = get_empty_before(i)
-        if not empty_spot == -1:
-            swap(i, empty_spot)
-        else:
-            break;
+ 
+for block in reversed(blocks):
+    empty_spot = spot_available(block[1])
+    if not empty_spot == -1:
+        move(block, empty_spot)
 
 checksum = 0
 for i in range(0, len(data)):
